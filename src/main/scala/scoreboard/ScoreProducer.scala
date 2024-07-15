ea33aa6420f14661e4c5e94fce4f909a54cdd8fd
jmarusak <jmarusak@users.noreply.github.com>
2024-07-15 13:30:22 -0400

    logging slf4j update

--- src/main/scala/scoreboard/ScoreProducer.scala
+++ src/main/scala/scoreboard/ScoreProducer.scala
@@ -1,7 +1,6 @@
 package scoreboard
 
 import spray.json._
-import org.slf4j.LoggerFactory
 
 import scoreboard.Score
 import scoreboard.ScoreJsonProtocol._
@@ -10,27 +9,25 @@ import scoreboard.KafkaClient
 import scoreboard.KafkaMockup
 
 object ScoreProducer {
-  private val logger = LoggerFactory.getLogger(getClass)
-
   def parseArgs(args: Array[String]): (String, String, String) = {
     if (args.length != 6) {
       println("Usage: scoreboard.ScoreProducer --court <number> --A <points> --B <points>")
       System.exit(1)
     }
 
-  var court, a, b = ""
+    var court, a, b = ""
 
-  args.sliding(2, 2).toList.collect {
-    case Array("--court", arg: String) => court = arg
-    case Array("--A", arg: String) => a = arg
-    case Array("--B", arg: String) => b = arg
+    args.sliding(2, 2).toList.collect {
+      case Array("--court", arg: String) => court = arg
+      case Array("--A", arg: String) => a = arg
+      case Array("--B", arg: String) => b = arg
+    }
+
+    (court, a, b)
   }
 
-  (court, a, b)
-}
-
   def main(args: Array[String]): Unit = {
-    logger.info("ScoreProducer started...")
+    println("ScoreProducer started...")
 
     val (court, a, b) = parseArgs(args)
     val score = Score(court, a.toInt, b.toInt)
